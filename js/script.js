2fab7f5c9098f66f014447369be34bcc56137bb7
bukhuti-vardanidze <vardanidzebuta6@gmail.com>
2021-06-24 19:47:21 +0400

    add slides

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-             /* burger  */
-  
+             /* slider  */
+ /*   var counter = 1;
+    setInterval(function(){
+        document.getElementById('radio'+counter).checked=true;
+        counter++;
+        if(counter > 4){
+           counter=1;
+        }
+    }, 5000);
+  */
