6824ab6434e15914ec1e1f6d6a7dfb39ce42a1a3
bukhuti-vardanidze <vardanidzebuta6@gmail.com>
2021-06-26 15:29:17 +0400

    project done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-             /* slider  */
+        
+     let navbarLinks = document.getElementById('navBarlinks');
+     let Burger = document.getElementById('burgerbar');
+
+     Burger.addEventListener('click', function()  {
+           navbarLinks.classList.toggle('active');
+        })
+        
+        
+        
+        
+        
+        
+        
+        /* slider  */
  /*   var counter = 1;
     setInterval(function(){
         document.getElementById('radio'+counter).checked=true;
