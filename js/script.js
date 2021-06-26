942aafea7e15e5c7bff78e05bf6b06a4cb8265fc
bukhuti-vardanidze <vardanidzebuta6@gmail.com>
2021-06-26 15:41:19 +0400

    done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
         
-     let navbarLinks = document.getElementById('navBarlinks');
+   /*  let navbarLinks = document.getElementById('navBarlinks');
      let Burger = document.getElementById('burgerbar');
 
      Burger.addEventListener('click', function()  {
            navbarLinks.classList.toggle('active');
         })
-        
+        */
         
         
         
